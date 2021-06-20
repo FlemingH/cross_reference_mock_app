b0538010fd3afe184d346a4214438828c608efdb
flm <lm.fei@outlook.com>
2021-06-20 21:38:33 +0800

    mock app use button to push message

--- app/src/main/java/com/volvo/crm/MainActivity.java
+++ app/src/main/java/com/volvo/crm/MainActivity.java
@@ -9,39 +9,81 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+import android.view.View;
 
 import com.volvo.crs.IMyAidlInterface;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "client";
+
+    //由AIDL文件生成的Java类
+    private IMyAidlInterface iMyAidlInterface = null;
+
+    //标志当前与服务端连接状况的布尔值，false为未连接，true为连接中
+    private boolean mBound = false;
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
 
-            try {
-                Log.d("AIDL return: ", "sum is " +
-                        IMyAidlInterface.Stub.asInterface(service).add(1, 2));
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
+            Log.e(TAG, "service connected");
+            iMyAidlInterface = IMyAidlInterface.Stub.asInterface(service); // init manager
+            mBound = true;
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            Log.e(TAG, "service disconnected");
+            mBound = false;
         }
     };
+
+    private void attemptToBindService() {
+        Intent intent = new Intent();
+        intent.setAction("com.volvo.crs.crservice");
+        intent.setPackage("com.volvo.crs");
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+
+    // 按钮点击事件
+    public void aidlPush(View view) {
+
+        if (!mBound) {
+            attemptToBindService();
+            return;
+        }
+
+        try {
+            iMyAidlInterface.add(1, 2);
+        } catch (RemoteException e) {
+            Log.e(TAG, "aidlPush RemoteException");
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBound) {
+            attemptToBindService();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(serviceConnection);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent();
-        intent.setAction("com.volvo.crs.crservice");
-        intent.setPackage("com.volvo.crs");
-        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
-
     }
 }
