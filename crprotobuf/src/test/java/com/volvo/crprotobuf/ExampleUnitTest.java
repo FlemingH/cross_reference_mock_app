a2e59dfe4563d597de711d5f342e3c1c5fcfc636
flm <lm.fei@outlook.com>
2021-07-09 17:10:07 +0800

    build protobuf java class

--- crprotobuf/src/test/java/com/volvo/crprotobuf/ExampleUnitTest.java
+++ crprotobuf/src/test/java/com/volvo/crprotobuf/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.volvo.crprotobuf;
 
-import org.junit.Test;
+import com.volvocars.crprotobuf.HdRoute;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,17 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testProtobuf() {
+
+        HdRoute.NavigationRoute navigationRoute = HdRoute.NavigationRoute.newBuilder().setLength(1).setLinknumber(1).addLinks(
+                HdRoute.Links.newBuilder().setLength(1).setLevel(1).setUsage(1).setNum(1).addLinkpt(
+                        HdRoute.Linkpt.newBuilder().setLat(1).setLon(1)
+                ).addLinkpt(
+                        HdRoute.Linkpt.newBuilder().setLat(2).setLon(2)
+                )
+        ).build();
+
+        System.out.println(navigationRoute);
+
     }
 }
